5b32e1fad3e4faa21b26274ba7fe9df0a4787e9e
Mario <maes0186@gmail.com>
2016-11-29 23:01:03 -0500

    Aplicativo nuevo de creditos

--- frontEnd/proyectoini/www/js/controllers.js
+++ frontEnd/proyectoini/www/js/controllers.js
@@ -45,28 +45,7 @@ angular.module('starter')
     $state.go('login');
   };
 
-  $scope.performValidRequest = function() {
-    $http.get('http://localhost:8100/valid').then(
-      function(result) {
-        $scope.response = result;
-      });
-  };
+ 
 
-  $scope.performUnauthorizedRequest = function() {
-    $http.get('http://localhost:8100/notauthorized').then(
-      function(result) {
-        // No result here..
-      }, function(err) {
-        $scope.response = err;
-      });
-  };
-
-  $scope.performInvalidRequest = function() {
-    $http.get('http://localhost:8100/notauthenticated').then(
-      function(result) {
-        // No result here..
-      }, function(err) {
-        $scope.response = err;
-      });
-  };
+ 
 });
