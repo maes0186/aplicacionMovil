bac9da6a0268e2135f987b527ff0a6f895e9a5aa
Mario <maes0186@gmail.com>
2016-12-01 19:15:57 -0500

    Ajustes de graficas

--- frontEnd/proyectoini/www/js/controllers.js
+++ frontEnd/proyectoini/www/js/controllers.js
@@ -20,6 +20,7 @@ $scope.datos = {};
     datos.valorCredito=AuthService.formatearValoresMoneda(AuthService.obtenerValorCredito(datos));
     datos.vIniValorInicial=datos.vApartamento*datos.pCuotaInicial/100;
     datos.iniValorInicial=AuthService.formatearValoresMoneda(datos.vIniValorInicial);
+    
   $scope.datos=datos; 
   };
 
@@ -38,7 +39,26 @@ $scope.datos = {};
    return AuthService.calcularCuota(datos,tasa);
  };
  $scope.formatearValoresMoneda = function(valor) {
+   if(isNaN(valor))return '$0';
    return AuthService.formatearValoresMoneda(valor);
  };
+
+ $scope.init = function () {
+ $scope.labels2 = ['Tasa1', 'Tasa2', 'Tasa3', 'Tasa4'];
+ $scope.series2 = ['Tasas'];
+
+  $scope.data2 = [
+    [$scope.obtenerCuota($scope.datos,$scope.datos.tasa1),
+    $scope.obtenerCuota($scope.datos,$scope.datos.tasa2),
+    $scope.obtenerCuota($scope.datos,$scope.datos.tasa3),
+    $scope.obtenerCuota($scope.datos,$scope.datos.tasa4)]
+  ];
+}
+
+$scope.init();
+
+
+
 })
 ;
+
